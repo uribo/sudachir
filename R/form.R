970f37222433440ef1bb6bdf733b9484de4c551d
Shinya Uryu <suika1127@gmail.com>
2020-10-23 15:41:33 +0900

    Several output form

--- R/form.R
+++ R/form.R
@@ -1,32 +1,61 @@
 #' Parse tokenized input text
 #'
 #' @inheritParams tokenizer
-#' @param type return form. "dictionary" or "normalized".
+#' @param type return form. One of the following "surface", "dictionary",
+#' "normalized", "reading" or "part_of_speech".
+#' @param pos Include part of speech information with object name.
 #' @export
-form <- function(x, mode, type) {
+form <- function(x, mode, type, pos = TRUE) {
   type <-
     rlang::arg_match(type,
-                     c("dictionary", "normalized"))
+                     c("surface",
+                       "dictionary",
+                       "normalized",
+                       "reading",
+                       "part_of_speech"))
   fn <-
     switch (type,
-            "dictionary" = dictionary_form_vec,
-            "normalized" = normalized_form_vec
-    )
+            "surface" = "surface",
+            "dictionary" = "dictionary_form",
+            "normalized" = "normalized_form",
+            "reading" = "reading_form",
+            "part_of_speech" = "part_of_speech")
   purrr::map(tokenizer(x, mode),
-             fn)
+             form_vec,
+             type = fn,
+             pos = pos)
 }
 
-dictionary_form_vec <- function(x) {
-  purrr::set_names(
-    purrr::map_chr(seq.int(reticulate::py_len(x)),
-                   ~ x[.x - 1]$dictionary_form()),
-    purrr::map_chr(
-      seq.int(reticulate::py_len(x)),
-      ~ x[.x - 1]$part_of_speech()[1]
-    ))
-}
-
-normalized_form_vec <- function(x) {
-  purrr::map_chr(seq.int(reticulate::py_len(x)),
-                 ~ x[.x - 1]$normalized_form())
+form_vec <- function(x, type, pos = TRUE) {
+  type <-
+    rlang::arg_match(type,
+                     c("surface",
+                       "dictionary_form",
+                       "normalized_form",
+                       "reading_form",
+                       "part_of_speech"))
+  if (type != "part_of_speech") {
+    res <-
+      purrr::map_chr(seq.int(reticulate::py_len(x)),
+                     ~ purrr::pluck(x[.x - 1],
+                                    type) %>% {
+                                      .()
+                                    })
+    if (pos == TRUE) {
+      res <-
+        purrr::set_names(
+        res,
+        purrr::map_chr(
+          seq.int(reticulate::py_len(x)),
+          ~ x[.x - 1]$part_of_speech()[1]))
+    }
+  } else {
+    res <-
+      purrr::map(seq.int(reticulate::py_len(x)),
+                 ~ purrr::pluck(x[.x - 1],
+                                type) %>% {
+                                  .()
+                                })
+  }
+  res
 }
