d27924f37919c7bdbbbc17c7d7a9d98f8b012ef9
paithiov909 <am11.44@gmail.com>
2023-01-07 17:35:42 +0900

    tweak test

--- tests/testthat/utils.R
+++ tests/testthat/utils.R
@@ -1,5 +1,6 @@
 skip_if_no_sudachi <- function() {
-  if (!reticulate::py_module_available("sudachipy")) {
+  if (!all(reticulate::py_module_available("sudachipy"),
+           reticulate::py_module_available("sudachidict_core"))) {
     skip("SudachiPy not available for testing")
   }
 }
